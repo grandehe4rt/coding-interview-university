7928183b3a4c9779a89fef2db3ac6227a9114a4a
Gabriel Dias Mazieri <gabrieldias7200@gmail.com>
2023-06-17 10:41:04 -0300

    New checkpoint! Sat 10:41 Jun 17 2023

--- kata/big-O-Notation/go/main.go
+++ kata/big-O-Notation/go/main.go
@@ -10,45 +10,60 @@ import (
 // O(log N)
 // O(n log n)
 
-var theArray []int = []int{1, 2, 3, 4, 5}
-var arraySize int
-var itemsInArray int = 0
+// var theArray []int
+// var arraySize int
+// var itemsInArray int
 var startTime int64
 var endTime int64
 
 func main() {
-	// fmt.Println(linearSearchForValue(5))
-	linearSearchForValue(100)
+	b := &BigONotation{}
+	result := b.addItemToArray(5)
+	fmt.Println(result)
 }
 
-func generateRandomArray() {
-	for i := 0; i < arraySize; i++ {
-		arraySlice[i] = rand.Intn(1000) + 10
+type BigONotation struct {
+	theArray     []int
+	arraySize    int
+	itemsInArray int
+}
+
+func NewBigONotation(size int) *BigONotation {
+	return &BigONotation{
+		arraySize:    size,
+		theArray:     make([]int, size),
+		itemsInArray: 0,
+	}
+}
+
+func (b *BigONotation) generateRandomArray() {
+	for i := 0; i < b.arraySize; i++ {
+		b.theArray[i] = rand.Intn(1000) + 10
 	}
 }
 
 // O(1)
-func addItemToArray(newItem int) []int {
+func (b *BigONotation) addItemToArray(newItem int) []int {
 	// Literalmente adiciona um elemento ao Array (Pika)
-	return append(theArray, newItem)
+	return append(b.theArray, newItem)
 }
 
 // O(N)
-func linearSearchForValue(value int) {
+func (b *BigONotation) linearSearchForValue(value int) {
 	var valueInArray bool = false
 	var indexsWithValues string = ""
 
 	startTime = time.Now().UnixMilli()
 
-	for i := 0; i < arraySize; i++ {
-		if theArray[i] == value {
+	for i := 0; i < b.arraySize; i++ {
+		if b.theArray[i] == value {
 			valueInArray = true
 			indexsWithValues += fmt.Sprint(i) + " "
 		}
 	}
 
-	endTime = time.Now().UnixMilli()
 	fmt.Println("Value found: ", valueInArray)
+	endTime = time.Now().UnixMilli()
 	fmt.Println("Linear Search Took ", fmt.Sprint(endTime-startTime))
 
 }
